95969c324c8b522efd9cf14678a204ee6c741575
huma23 <manuel.hubert7@gmail.com>
2017-11-03 14:05:06 +0100

    added testsuite hierarchy

---
+++ src/test/java/org/wahlzeit/testEnvironmentProvider/TestEnvironmentProviderTestSuite.java
@@ -0,0 +1,41 @@
+/*
+ * author: huma23, github.com/huma23/
+ *
+ * This file is part of the Wahlzeit photo rating application.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package org.wahlzeit.testEnvironmentProvider;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * A test suite for all test in the package org.wahlzeit.testEnvironmentProvider
+ * 
+ * author: huma23
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	LocalDatastoreServiceTestConfigProvider.class,
+	RegisteredOfyEnvironmentProvider.class,
+	SysConfigProvider.class,
+	UserServiceProvider.class,
+	UserSessionProvider.class,
+	WebFormHandlerProvider.class
+})
+
+public class TestEnvironmentProviderTestSuite {
+}
