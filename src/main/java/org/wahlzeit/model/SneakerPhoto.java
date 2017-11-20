02fb202646c2842388d84e82112c79d45de18c18
huma23 <manuel.hubert7@gmail.com>
2017-11-20 01:01:58 +0100

    Some edits from last reviews

--- src/main/java/org/wahlzeit/model/SneakerPhoto.java
+++ src/main/java/org/wahlzeit/model/SneakerPhoto.java
@@ -1,9 +1,36 @@
+/*
+ * Author: huma23, github.com/huma23
+ * 
+ * This file is part of the Wahlzeit photo rating application.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 package org.wahlzeit.model;
 
 import com.googlecode.objectify.annotation.Subclass;
 
-@Subclass(name="Photo", alsoLoad="Photo", index=true)
+/**
+ * Class for representing a photo of sneaker wahlzeit
+ * @author huma23
+ *
+ */
+@Subclass
 public class SneakerPhoto extends Photo {
+	
+	private String brand;
+	private String color;
 
 	public SneakerPhoto() {
 		super();
@@ -11,5 +38,21 @@ public class SneakerPhoto extends Photo {
 	
 	public SneakerPhoto(PhotoId id) {
 		super(id);
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
 	}
 }
